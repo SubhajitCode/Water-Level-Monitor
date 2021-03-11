55ec00ecda4618f911da6445d3ac3b454e0bccc8
SubhajitCode <subhajitbarh93@gmail.com>
2021-03-11 12:50:33 +0530

    using multistream to reset the empty and full level

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 FirebaseData dbIn;
 FirebaseData dbOut;
 // Real-time database secret key here
-const int tankFullLevel = 30; //cm
-const int tankEmptyLevel = 300;
-const int maxHeight = 270;
+ int tankFullLevel = 7; //cm
+ int tankEmptyLevel = 78;
 const int trigPin = D7;
 const int echoPin = D8;
 const int relayPin = D5;
@@ -32,19 +31,18 @@ int ultraSonicRead();
 int calcPercent(int level);
 void drawPercentbar(int x, int y, int width, int height, int progress);
 void displayOled(int waterLevel);
-void streamCallback(StreamData data);
+void streamCallback(MultiPathStreamData  data);
 
-void setup()
-{
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3D (for the 128x64)
+String childPath[3] = {"/motorStat", "/tankEmptyLevel", "/tankFullLevel"};
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
   //display.setFont(&FreeSerif9pt7b);
-  Serial.begin(9600);
-  Serial.println("Communication Started \n\n");
+  Serial.begin(9600); Serial.println("Communication Started \n\n");
   ultraSonic.begin();
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+  pinMode(echoPin, INPUT);// Sets the echoPin as an Input
   pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW); //  Sets the motorPin as an Output
+  digitalWrite(relayPin, LOW);//  Sets the motorPin as an Output
   // connect to firebase
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -63,9 +61,11 @@ void setup()
   Firebase.setReadTimeout(dbIn, 1000 * 5);
   Firebase.setwriteSizeLimit(dbOut, "tiny");
 
+
   path = "/Test";
-  Firebase.setStreamCallback(dbIn, streamCallback, streamTimeoutCallback);
-  if (!Firebase.beginStream(dbIn, path + "/motorStat"))
+  size_t childPathSize = 3;
+  Firebase.setMultiPathStreamCallback(dbIn, streamCallback, streamTimeoutCallback);
+  if (!Firebase.beginMultiPathStream(dbIn, path, childPath, childPathSize ))
   {
     Serial.println("Could not begin stream");
     Serial.println("REASON: " + dbIn.errorReason());
@@ -75,34 +75,34 @@ void setup()
 
 void streamTimeoutCallback(bool timeout)
 {
-  if (timeout)
-  {
+  if (timeout) {
     //Stream timeout occurred
     Serial.println("Stream timeout, resume streaming...");
   }
 }
-void streamCallback(StreamData data)
-{
-  if (data.dataType() == "boolean")
-  {
-    if (!data.boolData())
-    {
+void streamCallback(MultiPathStreamData  data) {
+  if (data.get(childPath[0])) {
+    if (data.value!="true") {
       relayOn();
       relayOff();
     }
   }
+  if (data.get(childPath[1])) {
+    tankEmptyLevel = data.value.toInt();
+  }
+  if (data.get(childPath[2])) {
+    tankFullLevel = data.value.toInt();
+  }
+
 }
 
-void relayOn()
-{
-
-  if (!relayState)
-  {
+void relayOn() {
+  //Motor Cut-Off
+  if (!relayState) {
     Serial.println("On");
     relayState = true;
     digitalWrite(relayPin, HIGH);
-    if (!Firebase.setBool(dbOut, path + "/motorStat", false))
-    {
+    if (!Firebase.setBool(dbOut, path + "/motorStat", false)) {
       Serial.println("FAILED");
       Serial.println("REASON: " + dbOut.errorReason());
     }
@@ -110,23 +110,21 @@ void relayOn()
   }
 }
 
-void relayOff()
-{
-  if (relayState)
-  {
+void relayOff() {
+  if (relayState) {
     Serial.println("Off");
     relayState = false;
     digitalWrite(relayPin, LOW);
-    if (!Firebase.setBool(dbOut, path + "/motorStat", true))
-    {
+    if (!Firebase.setBool(dbOut, path + "/motorStat", true)) {
       Serial.println("FAILED");
       Serial.println("REASON: " + dbOut.errorReason());
     }
+
   }
 }
 
-int ultraSonicRead()
-{
+
+int ultraSonicRead() {
   // ULTRASONIC *****************************************
   int avg;
   long duration;
@@ -147,10 +145,11 @@ int ultraSonicRead()
   return avg;
 }
 
-int calcPercent(int level)
-{
+int calcPercent(int level) {
   int height = tankEmptyLevel - level;
+  int maxHeight=tankEmptyLevel - tankFullLevel;
   return (height * 100 / maxHeight);
+
 }
 
 void drawPercentbar(int x, int y, int width, int height, int progress)
@@ -159,57 +158,51 @@ void drawPercentbar(int x, int y, int width, int height, int progress)
   progress = progress < 0 ? 0 : progress;
   float bar = ((float)(width - 4) / 100) * progress;
   display.drawRect(x, y, width, height, WHITE);
-  display.fillRect(x + 2, y + 2, bar, height - 4, WHITE);
+  display.fillRect(x + 2, y + 2, bar , height - 4, WHITE);
   // Display progress text
-  if (height >= 15)
-  {
-    display.setCursor((width / 2) - 3, y + 5);
+  if ( height >= 15) {
+    display.setCursor((width / 2) - 3, y + 5 );
     display.setTextSize(1);
     display.setTextColor(WHITE);
-    if (progress >= 50)
+    if ( progress >= 50)
       display.setTextColor(BLACK, WHITE); // 'inverted' text
     display.print(progress);
     display.print("%");
   }
 }
 
-void displayOled(int waterLevel)
-{
+void displayOled(int waterLevel) {
   int val = calcPercent(waterLevel);
   display.clearDisplay();
-  char buffer[50];
-  sprintf(buffer, "%03d", val);
+  char buffer [50];
+  sprintf (buffer, "%03d", val);
   display.drawRect(0, 0, 128, 45, WHITE);
   display.setTextSize(4);
   display.setTextColor(WHITE);
   display.setCursor(18, 9);
   display.print(buffer);
   display.println('%');
-  drawPercentbar(0, 50, 128, 10, val > 100 ? 100 : val);
+  drawPercentbar( 0, 50, 128, 10, val > 100 ? 100 : val);
   display.display();
   Serial.print("value ");
   Serial.println(waterLevel);
   Serial.print("percent ");
   Serial.println(val);
   delay(100);
-  if (!Firebase.setInt(dbOut, path + "/waterLevel", waterLevel))
-  {
+  if (!Firebase.setInt(dbOut, path + "/waterLevel", waterLevel)) {
     Serial.println("FAILED");
     Serial.println("REASON: " + dbOut.errorReason());
   }
 }
 
-void loop()
-{
+void loop() {
   waterLevel = ultraSonicRead();
   // Prints the distance on the Serial Monitor
   displayOled(waterLevel);
-  if (waterLevel < tankFullLevel)
-  {
+  if (waterLevel < tankFullLevel) {
     relayOn();
     relayOff();
-    while (waterLevel < tankFullLevel)
-    {
+    while (waterLevel < tankFullLevel) {
       waterLevel = ultraSonicRead();
       displayOled(waterLevel);
     }
